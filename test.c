90192913c97c776568be112efb0dec9b292a9a7b
Herrfalco <cadet.florian@gmail.com>
2023-09-01 20:42:46 +0200

    checksum is working

--- test.c
+++ test.c
@@ -6,16 +6,17 @@
 /*   By: fcadet <fcadet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 09:16:03 by fcadet            #+#    #+#             */
-/*   Updated: 2023/08/31 21:25:32 by fcadet           ###   ########.fr       */
+/*   Updated: 2023/09/01 20:41:53 by fcadet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #define BUFF_SZ				4096
 #define MIN_PORT			49152
 #define	MAX_PORT			65536	
-#define DST_PORT			80
-#define DST_IP 				"91.211.165.100"
-#define DEF_WIN_SZ			5840
+#define SRC_PORT			55555
+#define DST_PORT			22
+#define DST_ADDR			"127.0.0.1"
+#define MAX_WIN				0xff
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,6 +29,7 @@
 #include <signal.h>
 #include <ifaddrs.h>
 #include <errno.h>
+#include <pthread.h>
 
 int					sock;
 
@@ -45,7 +47,7 @@ unsigned short checksum(uint16_t *data, uint64_t nbytes) {
     uint64_t	 sum;
 
 	for (sum = 0; nbytes; nbytes -= 2, sum += *(data++));
-	for (; sum >> 16; sum = (sum & 0xffff) + (sum >> 16));
+	for (; sum >> 16; sum = (sum & htonl(0xffff)) + (sum >> 16));
     return (~sum);
 }
 
@@ -55,70 +57,77 @@ void	sig_handler(int) {
 	exit(0);
 }
 
-int		bind_2_local(struct sockaddr_in *local) {
+int		get_local(struct in_addr *local, in_port_t *port) {
     struct ifaddrs		*ifaddr, *i;
-	uint32_t			port;
 
     if (getifaddrs(&ifaddr) == -1)
 		return (-1);
-	local->sin_family = AF_INET;
     for (i = ifaddr; i != NULL; i = i->ifa_next) {
 		if (i->ifa_addr && i->ifa_addr->sa_family == AF_INET
 				&& !(i->ifa_flags & IFF_LOOPBACK) && (i->ifa_flags & IFF_UP)
 				&& (i->ifa_flags & IFF_RUNNING)) {
-			local->sin_addr = ((struct sockaddr_in *)i->ifa_addr)->sin_addr;
-			break;
+			*local = ((struct sockaddr_in *)i->ifa_addr)->sin_addr;
+			*port = htons(SRC_PORT);
+			freeifaddrs(ifaddr);
+			return (0);
 		}
     }
 	freeifaddrs(ifaddr);
-	if (!i) {
-		errno = EADDRNOTAVAIL;
-		return (-1);
-	}
-	for (port = MIN_PORT; port < MAX_PORT; ++port) {
-		local->sin_port = htons(port);
-		if (!bind(sock, (struct sockaddr *)local, sizeof(struct sockaddr_in)))
-			return (0);
-	}
 	errno = EADDRNOTAVAIL;
 	return (-1);
 }
 
-int		print_addr_and_port(void) {
-	struct sockaddr_in		result = { 0 };
-	uint64_t				sz = sizeof(result);
+void		print_packet(void *packet) {
+	struct iphdr			*ip = (struct iphdr *)packet;
+	struct tcphdr			*tcp = (struct tcphdr *)(ip + 1);
 
-	if (getsockname(sock, (struct sockaddr *)&result, (socklen_t *)&sz))
-		return (-1);
-	printf("addr: %s, port: %d\n", inet_ntoa(result.sin_addr), ntohs(result.sin_port));
-	return (0);
+	printf("%s::%d > ",
+			inet_ntoa(*(struct in_addr *)&ip->saddr),
+			ntohs(tcp->source));
+	printf("%s::%d",
+			inet_ntoa(*(struct in_addr *)&ip->daddr),
+			ntohs(tcp->dest));
+	printf("%s%s%s%s%s%s\n",
+			tcp->fin ? " FIN" : "",
+			tcp->syn ? " SYN" : "",
+			tcp->rst ? " RST" : "",
+			tcp->psh ? " PSH" : "",
+			tcp->ack ? " ACK" : "",
+			tcp->urg ? " URG" : ""
+		  );
 }
 
-void	response(void) {
-	struct sockaddr_in		addr;
-	uint64_t				sz;
+void	*sniffer_fn(void *) {
 	char					buff[BUFF_SZ];
-	ssize_t					ret;
+	struct iphdr			*ip = (struct iphdr *)buff;
+	struct tcphdr			*tcp = (struct tcphdr *)(ip + 1);
 
-	if ((ret = recvfrom(sock, buff, BUFF_SZ, 0, (struct sockaddr *)&addr, (socklen_t *)&sz) < 0))
-		fprintf(stderr, "recv error\n");
-	printf("size: %ld, addr: %s, port: %d\n", sz, inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+	for (int i = 10; i; --i) {
+		if (recvfrom(sock, buff, BUFF_SZ, 0, NULL, NULL) < 0)
+			fprintf(stderr, "recv error\n");
+		if (ntohs(tcp->dest) == SRC_PORT) {
+			print_packet(buff);
+			return (NULL);
+		}
+	}
+	return (NULL);
 }
 
 int		main(void) {
-	int						on = 1;
+	int					on = 1;
     char				data[BUFF_SZ] = { 0 };
     struct iphdr		*iph = (struct iphdr *)data;
     struct tcphdr		*tcph = (struct tcphdr *)(iph + 1);
 	ip_pseudo_t			*ipp = ((ip_pseudo_t *)tcph) - 1;
-	struct sockaddr_in	local = { 0 }, remote = {
+//	pthread_t			sniffer;
+	struct sockaddr_in	local = {
 		.sin_family = AF_INET,
-		.sin_addr.s_addr = inet_addr(DST_IP),
+	}, remote = {
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = inet_addr(DST_ADDR),
 		.sin_port = htons(DST_PORT),
 	};
 
-	(void)remote;
-	(void)ipp;
 	if (signal(SIGINT, sig_handler) == SIG_ERR) {
         perror("Sig handler error");
         return (1);
@@ -132,41 +141,50 @@ int		main(void) {
         perror("Socket option error");
         return (3);
     }
-	if (bind_2_local(&local)) {
+	if (get_local(&local.sin_addr, &local.sin_port)) {
 		close(sock);
-		perror("Bind error");
+		perror("Local error");
 		return (4);
 	}
-	print_addr_and_port();
+	/*
+	if (pthread_create(&sniffer, NULL, sniffer_fn, NULL)) {
+		close(sock);
+		fprintf(stderr,
+				"Sniffer creation: Fail to create thread\n");
+	}
+	*/
 
 	ipp->src = local.sin_addr.s_addr;
-	ipp->dst = inet_addr(DST_IP);
+	ipp->dst = remote.sin_addr.s_addr;
 	ipp->prot = IPPROTO_TCP;
-	ipp->tcp_seg_sz = sizeof(struct tcphdr);
+	ipp->tcp_seg_sz = htons(sizeof(struct tcphdr));
 
     tcph->source = local.sin_port;
     tcph->dest = remote.sin_port;
     tcph->doff = sizeof(struct tcphdr) / 4;
     tcph->syn = 1;
-    tcph->check = checksum((uint16_t *)ipp, sizeof(ip_pseudo_t) + sizeof(struct tcphdr));
+	tcph->window = MAX_WIN;
+    tcph->check = checksum((uint16_t *)ipp,
+			sizeof(ip_pseudo_t) + sizeof(struct tcphdr));
 
 	bzero(iph, sizeof(struct iphdr));
     iph->version = 4;
     iph->ihl = sizeof(struct iphdr) / 4;
-	iph->tot_len = sizeof(struct iphdr) + sizeof(struct tcphdr);
     iph->ttl = 255;
     iph->protocol = IPPROTO_TCP;
 	iph->saddr = local.sin_addr.s_addr;
-    iph->daddr = inet_addr(DST_IP);
+    iph->daddr = remote.sin_addr.s_addr;
 
-    if (sendto(sock, data, iph->tot_len, 0, (struct sockaddr *)&remote, sizeof(struct sockaddr_in)) == -1) {
+    if (sendto(sock, data,
+				sizeof(struct iphdr) + sizeof(struct tcphdr),
+				0, (struct sockaddr *)&remote,
+				sizeof(struct sockaddr_in)) == -1) {
         perror("Sendto error");
         return (1);
     }
     printf("SYN packet sent.\n");
-
-	response();
-
+	sniffer_fn(NULL);
+//	pthread_join(sniffer, NULL);
 	close(sock);
 	return (0);
 }
