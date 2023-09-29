e47ef186d4364e5b427bdf513dbc680440f66a8e
apitoise <arthur.pitoiset@outlook.fr>
2023-09-29 12:03:36 +0200

    handle many signals

--- srcs/signal.c
+++ srcs/signal.c
@@ -8,14 +8,18 @@ static void		sig_action(int) {
 
 char			*handle_sig() {
 	sigset_t			mask;
-	int					err;
+	int					err, i, sig[] = {
+		SIGALRM, SIGHUP, SIGINT, SIGPIPE,
+		SIGTERM, SIGUSR1, SIGUSR2, SIGQUIT
+	};
 	struct sigaction	act = { 
 		.sa_handler = sig_action,
 	};
 
 	sigemptyset(&mask);
 	act.sa_mask = mask;
-	if ((err = sigaction(SIGINT, &act, NULL)))
-		return (strerror(err));
+	for (i = 0; i < 8; ++i)
+		if ((err = sigaction(sig[i], &act, NULL)))
+			return (strerror(err));
 	return (0);
 }
